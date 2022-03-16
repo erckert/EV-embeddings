90d021a5d7c8fa001d841c7615673288c5f56219
Kyra Erckert <kyra.erckert@tum.de>
2022-03-16 18:35:19 +0100

    Add print statement for config file  path

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import configparser
 import argparse
+import os
 
 class App:
     file_path = None
@@ -18,7 +19,8 @@ class App:
                                 help='File path to .ini file if it isn\'t ev_embeddings.ini',
                                 metavar="file_path")
             args = parser.parse_args()
-            App.file_path = args.f
+            App.file_path = os.path.normpath(args.f)
+            print(f'Used filepath is: {App.file_path}')
         if App._conf is None:
             App._conf = configparser.ConfigParser()
             App._conf.read(App.file_path)
