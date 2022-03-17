6c1665ad300aeb7bf6966a31c7c9ecfd195c555a
Kyra Erckert <kyra.erckert@tum.de>
2022-03-17 13:08:46 +0100

    Rename modelSelector to configProcessor, add model selection

--- setup/configProcessor.py
+++ setup/configProcessor.py
@@ -1,20 +1,23 @@
 import sys
 import os
+import torch
 
 from config import App
 
 embedding_models = ["SeqVec", "Bert", "ProtT5"]
 embedding_types = ["SingleSeq", "MSA"]
-evolutionary_informations = ["None", "MSAConsensus", "PSSM"]
+evolutionary_information_types = ["None", "MSAConsensus", "PSSM"]
 pssm_models = [0, 1]
 
 
 def select_model_from_config():
     config = App.config()
+
     if not is_valid_config(config):
         sys.exit("Config is invalid. Please provide a valid config file.")
-    select_model(config)
     print("Config file is valid")
+
+    return select_model(config)
 
 
 def is_valid_config(config):
@@ -29,7 +32,7 @@ def is_valid_config(config):
         print(f"Config file does not specify a valid embedding_model. Please select one of the following: "
               f"{embedding_models}")
         return False
-    if general_section.get("embedding_type").lower() not in map(lambda x:x.lower(), embedding_types):
+    if general_section.get("embedding_type").lower() not in map(lambda x: x.lower(), embedding_types):
         print(f"Config file does not specify a valid embedding_type. Please select one of the following: "
               f"{embedding_types}")
         return False
@@ -45,11 +48,10 @@ def is_valid_config(config):
 
     # Check if evolutionary information type is selected
     if config["SINGLE SEQUENCE EMBEDDINGS"]["evolutionary_information"].lower() not in \
-            map(lambda x: x.lower(), evolutionary_informations):
+            map(lambda x: x.lower(), evolutionary_information_types):
         print(f"Config file does not specify a valid evolutionary_information. Please select one of the following: "
-              f"{evolutionary_informations}")
+              f"{evolutionary_information_types}")
         return False
-    
 
     # If None has been selected as evolutionary_information all further entries can be ignored,
     # else check remaining relevant entries
@@ -76,5 +78,36 @@ def is_valid_config(config):
 
     return True
 
+
 def select_model(config):
-    return ""
+    config_general_section = config["GENERAL"]
+    embedding_model = config_general_section["embedding_model"].lower()
+    embedding_type = config_general_section["embedding_type"].lower()
+    evolutionary_information = config["SINGLE SEQUENCE EMBEDDINGS"]["evolutionary_information"].lower()
+
+    match embedding_model:
+        case "seqvec":
+            model_sub_folder = "seqVec"
+        case "bert":
+            model_sub_folder = "Bert"
+        case "prott5":
+            model_sub_folder = "ProtT5"
+        case _:
+            sys.exit(f"Error: No sub folder found for model {embedding_model}. Please contact the development team.")
+
+    model_folder_path = os.path.join("pretrained_models", model_sub_folder)
+
+    match embedding_type:
+        case "msa":
+            model_name = "pretrained_msa_embedding.pt"
+        case "single":
+            if evolutionary_information == "pssm":
+                model_name = f"pretrained_embedding_and_pssm_{config['PSSM']['model_selection']}.pt'"
+            else:
+                model_name = "pretrained_embedding_only.pt"
+        case _:
+            sys.exit(f'Error: No matching model "{embedding_model}" found. Please contact the development team.')
+
+    model = torch.load(os.path.join(model_folder_path, model_name))
+
+    return model
