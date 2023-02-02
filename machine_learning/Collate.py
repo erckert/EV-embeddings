16b95eadc558a78c179c06d4267e687cb06b7b34
Kyra Erckert <kyra.erckert@tum.de>
2023-02-02 14:00:08 +0100

    Add proteinCollate

---
+++ machine_learning/Collate.py
@@ -0,0 +1,8 @@
+from torch.nn.utils.rnn import pad_sequence
+
+
+class ProteinCollate(object):
+    def __call__(self, data, ignore_idx=-100):
+        inputs, protein_info = zip(*data)
+        inputs = pad_sequence(inputs, batch_first=True, padding_value=ignore_idx)
+        return inputs, protein_info
