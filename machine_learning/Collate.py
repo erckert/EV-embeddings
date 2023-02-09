f0c776e3c632563688cee957025400e64a70cb2b
Kyra Erckert <kyra.erckert@tum.de>
2023-02-09 14:10:54 +0100

    Format predictions for MSAConsensus, Update Collator and dependent code to work with MSAConsensus datasets

--- machine_learning/Collate.py
+++ machine_learning/Collate.py
@@ -3,6 +3,6 @@ from torch.nn.utils.rnn import pad_sequence
 
 class ProteinCollate(object):
     def __call__(self, data, ignore_idx=-100):
-        inputs, protein_info = zip(*data)
+        inputs, protein_info, query_name = zip(*data)
         inputs = pad_sequence(inputs, batch_first=True, padding_value=ignore_idx)
-        return inputs, protein_info
+        return inputs, protein_info, query_name
--- machine_learning/Predictions.py
+++ machine_learning/Predictions.py
@@ -6,6 +6,7 @@ from config import App
 
 
 def predict(device, model, dataloader):
+    print("Generating predictions")
     embedding_type = App.config()["GENERAL"]["embedding_type"].lower()
     evolutionary_information = App.config()["SINGLE SEQUENCE EMBEDDINGS"]["evolutionary_information"].lower()
 
@@ -18,7 +19,7 @@ def predict(device, model, dataloader):
 def predict_individual_sequences(device, model, dataloader):
     predictions = {}
     with torch.no_grad():
-        for i, (inputs, protein_names) in enumerate(dataloader):
+        for i, (inputs, protein_names, _) in enumerate(dataloader):
             output = model(inputs.to(device))
             network_predictions = [prediction.argmax(dim=0) for prediction in output]
 
@@ -33,14 +34,17 @@ def predict_individual_sequences(device, model, dataloader):
 def predict_consensus_from_msa(device, model, dataloader):
     predictions = {}
     with torch.no_grad():
-        for i, (inputs, query_names, protein_names) in enumerate(dataloader):
+        for i, (inputs, protein_names, query_names) in enumerate(dataloader):
             outputs = model(inputs.to(device))
             network_predictions = [prediction.argmax(dim=0) for prediction in outputs]
 
-            for network_prediction, query_name, protein_name in zip(network_predictions, query_names, protein_names):
+            for network_prediction, protein_name, query_name, network_input in \
+                    zip(network_predictions, protein_names, query_names, inputs):
                 if query_name not in predictions:
                     predictions[query_name] = {}
-                # TODO: remove masked residue predictions, format of prediction
-                predictions[query_name][protein_name] = network_prediction
+                network_prediction = [entry.item() for entry in network_prediction]
+                mask = [all(embedding_dim != -100 for embedding_dim in entry) for entry in
+                        [residue_input for residue_input in network_input]]
+                predictions[query_name][protein_name] = list(itertools.compress(network_prediction, mask))
 
     return predictions
